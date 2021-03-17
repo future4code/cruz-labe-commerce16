558f08fb823e03bcc33594b6cb4970c73db76a7d
TheusH7X5 <matheusa7x.23@gmail.com>
2021-03-17 15:34:50 -0300

    grid app.js ajustado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`;
 
 /*testando*/
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <div className="teste">teste1</div>
       <div className="teste">teste2</div>
       <div className="teste">teste3</div>
-    </div>
+    </AppContainer>
   );
 }
 
